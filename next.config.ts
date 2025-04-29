8b0037b6ab11f1e804a6e934eb5f8ff9c4e0d327
Leonardo Cech <cech@5cycle.com.br>
2025-04-29 00:29:02 -0300

    Correção de paths na configuração do Next para encontrar o CSS no GitHub pages

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true, // Necessário para exportação estática
-  },
-  basePath: '/brenda-gaio-website',
-  assetPrefix: '/brenda-gaio-website/'
+  }
 };
 
 export default nextConfig;
